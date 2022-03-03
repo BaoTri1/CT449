343a284c936956f56f8cbb03eab8ec5819d4acb7
unknown <BaoTri1>
2022-03-03 16:44:03 +0700

    Cai dat cac handler truy xuat CSDL

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const app = require("./app");
 const config = require("./app/config");
+const mongoose = require("mongoose");
 
+//Connect to database
+mongoose.connect(config.db.uri)
+    .then(() => {
+        console.log("Conneted to the database!");
+    })
+    .catch((error) => {
+        console.log("Cannot connet to the database!", error);
+    });
+
+// start server     
 const PORT = config.app.PORT;
 
 app.listen(PORT, () => {
